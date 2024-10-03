28ac71ceb9bc914340a82585aef0381247e9d4d0
gusstadeu <gtadeu.jg@gmail.com>
2024-10-03 18:14:55 -0300

    Beta 1.2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Calcule from './page/Calcule.jsx'
+import Header from './layout/Header.jsx'
 
 
 function App() {
   return (
     <div className="App">
+        <Header />
         <Calcule />
     </div>
   );
